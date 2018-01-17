5ed067450056c5a9c22926a22d11ef55c5b2bce8
Tomasz Idzi <idzitomek@gmail.com>
2018-01-17 16:50:28 +0100

    Added connections with UI to DetailForecastViewController. Added connection UI with web service data.

---
+++ WeatherTestApp/WeatherTestApp/ViewControllers/DetailForecastViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailForecastViewController.swift
+//  WeatherTestApp
+//
+//  Created by Tomasz Idzi on 17/01/2018.
+//  Copyright © 2018 Tomasz Idzi. All rights reserved.
+//
+
+import UIKit
+
+class DetailForecastViewController: UIViewController {
+
+    //MARK: Properties
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var morningLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var eveningLabel: UILabel!
+    @IBOutlet weak var nightLabel: UILabel!
+    
+    var forecastDailyWeather: ForecastDailyWeather!
+    
+    //MARK: Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI(with: forecastDailyWeather)
+    }
+    
+    //MARK: Actions
+    @IBAction func backPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Functions
+    func updateUI(with forecast: ForecastDailyWeather) {
+        if let temperature = forecast.temperature {
+            self.minLabel.text = temperature.min?.celsiusString
+            self.maxLabel.text = temperature.max?.celsiusString
+            self.morningLabel.text = temperature.morn?.celsiusString
+            self.dayLabel.text = temperature.day?.celsiusString
+            self.eveningLabel.text = temperature.eve?.celsiusString
+            self.nightLabel.text = temperature.night?.celsiusString
+        }
+    }
+}
