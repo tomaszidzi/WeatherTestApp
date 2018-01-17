5256295bd2416e8622370f914a42a4ca22c24902
Tomasz Idzi <idzitomek@gmail.com>
2018-01-17 13:11:10 +0100

    Implement TableView delegates in MainViewController.

--- WeatherTestApp/WeatherTestApp/ViewControllers/MainViewController.swift
+++ WeatherTestApp/WeatherTestApp/ViewControllers/MainViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    //MARK: Properties
     @IBOutlet weak var headerView: MainHeaderView!
+    @IBOutlet weak var forecastTableView: UITableView!
     
+    var forecastResponse : ForecastResponse?
+    
+    //MARK: Controller Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         APIManager.getWeather() { (response, error) in
             if let weatherResponse = response {
                 self.headerView.updateUI(with: weatherResponse)
@@ -22,15 +26,42 @@ class MainViewController: UIViewController {
         }
         
         APIManager.getForecast() { (response, error) in
-            
+            if let forecastDailyResponse = response {
+                self.forecastResponse = forecastDailyResponse
+                self.forecastTableView.reloadData()
+            }
         }
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//MARK: TableView
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let detail = self.forecastResponse?.details {
+            return detail.count
+        } else {
+            return 0
+        }
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "ForecastCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ForecastCell  else {
+            fatalError("The dequeued cell is not an instance of ForecastCell.")
+        }
+        
+        if let detail = self.forecastResponse?.details {
+            cell.updateUI(with: detail[indexPath.row])
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85.0
+    }
 }
 
