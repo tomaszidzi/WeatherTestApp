a257637194d6636acef6d7581abd98c6c1d79b80
Tomasz Idzi <idzitomek@gmail.com>
2018-01-17 11:30:04 +0100

    Added file for implementation of extension.

---
+++ WeatherTestApp/WeatherTestApp/Extensions/Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Extensions.swift
+//  WeatherTestApp
+//
+//  Created by Tomasz Idzi on 17/01/2018.
+//  Copyright © 2018 Tomasz Idzi. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    var celsius : Double {
+        return (self - 273.15).rounded()
+    }
+    
+    var timestampToLongDateString : String {
+        let date = Date(timeIntervalSince1970: self)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.locale = NSLocale.current
+        return dateFormatter.string(from: date)
+    }
+}
+
